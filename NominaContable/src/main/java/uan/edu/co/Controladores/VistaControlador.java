52989a839b1fd2f1517c093a04cfab2d60b49298
Andres Felipe Barragan <abarragan02@uan.edu.co>
2019-11-25 19:58:48 -0500

    Completando controlador con los demas empleados

--- NominaContable/src/main/java/uan/edu/co/Controladores/VistaControlador.java
+++ NominaContable/src/main/java/uan/edu/co/Controladores/VistaControlador.java
@@ -13,6 +13,18 @@ public class VistaControlador {
 	
 	Empleado empleado1 = new Empleado();
 	DevengadosAdiciones devengado1 = new DevengadosAdiciones();
+	
+	Empleado empleado2 = new Empleado();
+	DevengadosAdiciones devengado2 = new DevengadosAdiciones();
+	
+	Empleado empleado3 = new Empleado();
+	DevengadosAdiciones devengado3 = new DevengadosAdiciones();
+	
+	Empleado empleado4 = new Empleado();
+	DevengadosAdiciones devengado4 = new DevengadosAdiciones();
+	
+	Empleado empleado5 = new Empleado();
+	DevengadosAdiciones devengado5 = new DevengadosAdiciones();
 
 	@FXML
     private TextField sueldoEmp1;
@@ -214,27 +226,122 @@ public class VistaControlador {
     
     @FXML
     void calculoTotalNomina(ActionEvent event) {
+    	
+    	//Para empleado 1
     	empleado1.setNombre("empleado 1");
-    	int sueldo=Integer.parseInt(sueldoEmp1.getText());
-    	empleado1.setSueldoBasico(sueldo);
+    	int sueldo1=Integer.parseInt(sueldoEmp1.getText());
+    	empleado1.setSueldoBasico(sueldo1);
     	
     	devengado1.auxTrans(empleado1, devengado1);
     	
-    	double extrasDiurna = Double.parseDouble(exDiurEmp1.getText());
-    	devengado1.setExtraDiurna(extrasDiurna);
+    	double extrasDiurna1 = Double.parseDouble(exDiurEmp1.getText());
+    	devengado1.setExtraDiurna(extrasDiurna1);
     	devengado1.horaExtraDiurna(empleado1, devengado1);
     	
-    	double extrasNocturna = Double.parseDouble(exNoctuEmp1.getText());
-    	devengado1.setExtraNocturna(extrasNocturna);
+    	double extrasNocturna1 = Double.parseDouble(exNoctuEmp1.getText());
+    	devengado1.setExtraNocturna(extrasNocturna1);
     	devengado1.horaExtraNocturna(empleado1, devengado1);
     	
-    	double extrasFestivoDiurnos = Double.parseDouble(exDiurFesEmp1.getText());
-    	devengado1.setExtraFestivoDiurna(extrasFestivoDiurnos);
+    	double extrasFestivoDiurnos1 = Double.parseDouble(exDiurFesEmp1.getText());
+    	devengado1.setExtraFestivoDiurna(extrasFestivoDiurnos1);
     	devengado1.horaExtraFestivoDiurno(empleado1, devengado1);
     	
-    	double extrasFestivoNocturnos = Double.parseDouble(exNoctFesEmp1.getText());
-    	devengado1.setExtraFestivoDiurna(extrasFestivoNocturnos);
+    	double extrasFestivoNocturnos1 = Double.parseDouble(exNoctFesEmp1.getText());
+    	devengado1.setExtraFestivoDiurna(extrasFestivoNocturnos1);
     	devengado1.horaExtraFestivoNocturno(empleado1, devengado1);
+    	
+    	//Para empleado 2
+    	empleado2.setNombre("empleado 2");
+    	int sueldo2=Integer.parseInt(sueldoEmp2.getText());
+    	empleado1.setSueldoBasico(sueldo2);
+    	
+    	devengado2.auxTrans(empleado2, devengado2);
+    	
+    	double extrasDiurna2 = Double.parseDouble(exDiurEmp2.getText());
+    	devengado2.setExtraDiurna(extrasDiurna2);
+    	devengado2.horaExtraDiurna(empleado2, devengado2);
+    	
+    	double extrasNocturna2 = Double.parseDouble(exNoctuEmp2.getText());
+    	devengado2.setExtraNocturna(extrasNocturna2);
+    	devengado2.horaExtraNocturna(empleado2, devengado2);
+    	
+    	double extrasFestivoDiurnos2 = Double.parseDouble(exDiurFesEmp2.getText());
+    	devengado2.setExtraFestivoDiurna(extrasFestivoDiurnos2);
+    	devengado2.horaExtraFestivoDiurno(empleado2, devengado2);
+    	
+    	double extrasFestivoNocturnos2 = Double.parseDouble(exNoctFesEmp2.getText());
+    	devengado2.setExtraFestivoDiurna(extrasFestivoNocturnos2);
+    	devengado2.horaExtraFestivoNocturno(empleado2, devengado2);
+    	
+    	//Para empleado 3
+    	empleado3.setNombre("empleado 3");
+    	int sueldo3=Integer.parseInt(sueldoEmp3.getText());
+    	empleado3.setSueldoBasico(sueldo3);
+    	
+    	devengado3.auxTrans(empleado3, devengado3);
+    	
+    	double extrasDiurna3 = Double.parseDouble(exDiurEmp3.getText());
+    	devengado3.setExtraDiurna(extrasDiurna3);
+    	devengado3.horaExtraDiurna(empleado3, devengado3);
+    	
+    	double extrasNocturna3 = Double.parseDouble(exNoctuEmp3.getText());
+    	devengado3.setExtraNocturna(extrasNocturna3);
+    	devengado3.horaExtraNocturna(empleado3, devengado3);
+    	
+    	double extrasFestivoDiurnos3 = Double.parseDouble(exDiurFesEmp3.getText());
+    	devengado3.setExtraFestivoDiurna(extrasFestivoDiurnos3);
+    	devengado3.horaExtraFestivoDiurno(empleado3, devengado3);
+    	
+    	double extrasFestivoNocturnos3 = Double.parseDouble(exNoctFesEmp3.getText());
+    	devengado3.setExtraFestivoDiurna(extrasFestivoNocturnos3);
+    	devengado3.horaExtraFestivoNocturno(empleado3, devengado3);
+    	
+    	//Para empleado 4
+    	empleado4.setNombre("empleado 4");
+    	int sueldo4=Integer.parseInt(sueldoEmp4.getText());
+    	empleado4.setSueldoBasico(sueldo4);
+    	
+    	devengado4.auxTrans(empleado4, devengado4);
+    	
+    	double extrasDiurna4 = Double.parseDouble(exDiurEmp4.getText());
+    	devengado4.setExtraDiurna(extrasDiurna4);
+    	devengado4.horaExtraDiurna(empleado4, devengado4);
+    	
+    	double extrasNocturna4 = Double.parseDouble(exNoctuEmp4.getText());
+    	devengado4.setExtraNocturna(extrasNocturna4);
+    	devengado4.horaExtraNocturna(empleado4, devengado4);
+    	
+    	double extrasFestivoDiurnos4 = Double.parseDouble(exDiurFesEmp4.getText());
+    	devengado4.setExtraFestivoDiurna(extrasFestivoDiurnos4);
+    	devengado4.horaExtraFestivoDiurno(empleado4, devengado4);
+    	
+    	double extrasFestivoNocturnos4 = Double.parseDouble(exNoctFesEmp4.getText());
+    	devengado4.setExtraFestivoDiurna(extrasFestivoNocturnos4);
+    	devengado4.horaExtraFestivoNocturno(empleado4, devengado4);
+    	
+    	//Para empleado 5
+    	empleado5.setNombre("empleado 5");
+    	int sueldo5=Integer.parseInt(sueldoEmp5.getText());
+    	empleado5.setSueldoBasico(sueldo5);
+    	
+    	devengado5.auxTrans(empleado5, devengado5);
+    	
+    	double extrasDiurna5 = Double.parseDouble(exDiurEmp5.getText());
+    	devengado2.setExtraDiurna(extrasDiurna5);
+    	devengado5.horaExtraDiurna(empleado5, devengado5);
+    	
+    	double extrasNocturna5 = Double.parseDouble(exNoctuEmp5.getText());
+    	devengado5.setExtraNocturna(extrasNocturna5);
+    	devengado5.horaExtraNocturna(empleado5, devengado5);
+    	
+    	double extrasFestivoDiurnos5 = Double.parseDouble(exDiurFesEmp5.getText());
+    	devengado5.setExtraFestivoDiurna(extrasFestivoDiurnos5);
+    	devengado5.horaExtraFestivoDiurno(empleado5, devengado5);
+    	
+    	double extrasFestivoNocturnos5 = Double.parseDouble(exNoctFesEmp5.getText());
+    	devengado5.setExtraFestivoDiurna(extrasFestivoNocturnos5);
+    	devengado5.horaExtraFestivoNocturno(empleado5, devengado5);
+    	
     }
 
 }
