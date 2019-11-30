31f8770ae1746617c032d7649c75bada656132a5
timberious <36313827+timberious@users.noreply.github.com>
2019-11-30 01:05:39 -0500

    ajustes

--- NominaContable/src/main/java/uan/edu/co/NominaContable/App.java
+++ NominaContable/src/main/java/uan/edu/co/NominaContable/App.java
@@ -21,7 +21,7 @@ public class App extends Application
     
     @Override
     public void start(Stage stage) throws Exception{
-    	Parent root = FXMLLoader.load(getClass().getResource("/uan/edu/co/vista/Vista nomina.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("uan/edu/co/vista/VistaNomina.fxml"));
     	Scene scene = new Scene(root);
     	stage.setScene(scene);
     	stage.setResizable(false);
