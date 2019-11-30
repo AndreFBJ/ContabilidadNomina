12ffbf52ec67079e03432ae65347d6130643fe83
timberious <36313827+timberious@users.noreply.github.com>
2019-11-30 00:04:27 -0500

    se crea el ejecutable de la interfaz

--- NominaContable/src/main/java/uan/edu/co/NominaContable/App.java
+++ NominaContable/src/main/java/uan/edu/co/NominaContable/App.java
@@ -2,50 +2,30 @@ package uan.edu.co.NominaContable;
 
 import uan.edu.co.Controladores.*;
 import java.util.Scanner;
-public class App
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.stage.Stage;
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         
-        Empleado empleado1 = new Empleado();
-    	Empleado empleado2 = new Empleado();
-    	Empleado empleado3 = new Empleado();
-    	Empleado empleado4 = new Empleado();
-    	Empleado empleado5 = new Empleado();
-    	
-    	DevengadosAdiciones devengados = new DevengadosAdiciones();
-    	VistaControlador controller = new VistaControlador();
-    	
-    	empleado1.setNombre("empleado 1");
-    	empleado2.setNombre("empleado 2");
-    	empleado3.setNombre("empleado 3");
-    	empleado4.setNombre("empleado 4");
-    	empleado5.setNombre("empleado 5");
-    	
-    	empleado1.setSueldoBasico(500); // AQUI SE PONE EL GET-TEXT QUE HAGA EN EL DISEÑO
-    	empleado2.setSueldoBasico(500);
-    	empleado3.setSueldoBasico(500);
-    	empleado4.setSueldoBasico(100);
-    	empleado5.setSueldoBasico(100);
-    	
-    	DevengadosAdiciones auxilioTransporte = new DevengadosAdiciones();
-    	
-    	//AUXILIO DE TRANSPORTE EMPLEADO 1
-    	int auxEmpleado1= devengados.auxTrans(empleado1, auxilioTransporte);
-    	
-    	//AUXILIO DE TRANSPORTE EMPLEADO 2
-    	int auxEmpleado2= devengados.auxTrans(empleado2, auxilioTransporte);
-    	
-    	//AUXILIO DE TRNASPORTE EMPLEADO 3
-    	int auxEmpleado3= devengados.auxTrans(empleado3, auxilioTransporte);
-    	
-    	//AUXILIO DE TRNASPORTE EMPLEADO 4
-    	int auxEmpleado4= devengados.auxTrans(empleado4, auxilioTransporte);
-    	
-    	//AUXILIO DE TRANSPORTE EMPLEADO 5
-    	int auxEmpleado5= devengados.auxTrans(empleado5, auxilioTransporte);
+    	launch(args);
     	
     	
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception{
+    	Parent root = FXMLLoader.load(getClass().getResource("/uan/edu/co/vista/Vista nomina.fxml"));
+    	Scene scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.setResizable(false);
+    	stage.sizeToScene();
+    	stage.show();
     }
 }
